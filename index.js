f07ba37181c4d0db612e5083a08ee03b98b07be8
Your Name <you@example.com>
2024-10-29 20:34:34 +0600

    added vercel json

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello developer");
+  res.send("Hello developers");
 });
 
 app.listen(port, () => {

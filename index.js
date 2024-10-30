7d9abcda792455da97138412716b16feff971a15
Your Name <you@example.com>
2024-10-30 16:22:36 +0600

    added developer server

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const developersBd = client.db("developersHouse").collection("developers");
     const wishlistBd = client.db("developersHouse").collection("wishlist");
